2f18d705da1916b347a964d2d1e541449b0cb762
Gyumin Baek <guminb@ajou.ac.kr>
2023-09-12 06:44:25 +0900

    txt, json exception added

--- Analyzer.py
+++ Analyzer.py
@@ -138,38 +138,44 @@ def save_to_folder(generator, id, results, folder_name):
 
 # result_dict 딕셔너리를 가독성 좋게 txt 파일에 저장하는 함수입니다.
 def save_results_to_file(id_folder_path, id, results):
-    # txt 파일에 저장하는 부분
-    with open(os.path.join(id_folder_path, f"{id}_result.txt"), 'w') as f:
-        for Binary_Path, result_dict in results.items():
-            f.write("########################################################################################\n")
-            f.write(f"Binary_Path: {Binary_Path}\n")
+    try:
+        # txt 파일에 저장하는 부분
+        with open(os.path.join(id_folder_path, f"{id}_result.txt"), 'w') as f:
+            for Binary_Path, result_dict in results.items():
+                f.write("########################################################################################\n")
+                f.write(f"Binary_Path: {Binary_Path}\n")
 
-            f.write(f"\nID: {result_dict['id']}")
-            f.write(f"\nCompiler: {result_dict['compiler']}")
-            f.write(f"\nOptimization Level: {result_dict['optimization_level']}")
-            f.write(f"\nCode Generator: {result_dict['generator']}")
-            
-            f.write("\n\nCompile:\n")
-            for key, value in result_dict['compile'].items():
-                if key == 'error_message' and value is not None:
-                    f.write("\tError Message:\n")
-                    f.write("\t-----\n")
-                    for line in value.split('\n'):
-                        f.write(f"\t\t{line}\n")
-                    f.write("\t-----\n")
-                else:
-                    f.write(f"\t{key.capitalize()}: {value}\n")
+                f.write(f"\nID: {result_dict['id']}")
+                f.write(f"\nCompiler: {result_dict['compiler']}")
+                f.write(f"\nOptimization Level: {result_dict['optimization_level']}")
+                f.write(f"\nCode Generator: {result_dict['generator']}")
                 
-            f.write("\nRun:\n")
-            for key, value in result_dict['run'].items():
-                if key == 'error_message' and value is not None:
-                    f.write("\tError Message:\n")
-                    f.write("\t-----\n")
-                    for line in value.split('\n'):
-                        f.write(f"\t\t{line}\n")
-                    f.write("\t-----\n")
-                else:
-                    f.write(f"\t{key.capitalize()}: {value}\n")
+                f.write("\n\nCompile:\n")
+                for key, value in result_dict['compile'].items():
+                    if key == 'error_message' and value is not None:
+                        f.write("\tError Message:\n")
+                        f.write("\t-----\n")
+                        for line in value.split('\n'):
+                            f.write(f"\t\t{line}\n")
+                        f.write("\t-----\n")
+                    else:
+                        f.write(f"\t{key.capitalize()}: {value}\n")
+                    
+                f.write("\nRun:\n")
+                for key, value in result_dict['run'].items():
+                    if key == 'error_message' and value is not None:
+                        f.write("\tError Message:\n")
+                        f.write("\t-----\n")
+                        for line in value.split('\n'):
+                            f.write(f"\t\t{line}\n")
+                        f.write("\t-----\n")
+                    else:
+                        f.write(f"\t{key.capitalize()}: {value}\n")
+    except Exception as e:
+        print(f"An error occurred while writing txt file: {e}")
     # JSON 파일에 저장하는 부분
-    with open(os.path.join(id_folder_path, f"{id}_result.json"), 'w') as f:
-        json.dump(results, f, indent=4)
+    try:
+        with open(os.path.join(id_folder_path, f"{id}_result.json"), 'w') as f:
+            json.dump(results, f, indent=4) 
+    except Exception as e:
+        print(f"An error occurred while writing the json file: {e}")
