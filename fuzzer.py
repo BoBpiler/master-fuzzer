e8a4f8f93836a28c71a0ab233311352f0c799847
Gyumin Baek <guminb@ajou.ac.kr>
2023-10-05 18:41:33 +0900

    feat : update forkserver code

---
+++ fuzzer.py
@@ -0,0 +1,136 @@
+import subprocess
+import concurrent.futures
+import json
+from config import*
+from analyzer import*
+from running_system import*
+import logging
+import uuid
+import threading
+import queue
+import time
+import signal
+
+
+class Compiler:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+        self.process = None
+        self.compile_time_out = "5\n"
+    
+    def sync_write_data(self, data:str) :
+        self.process.stdin.write(data)
+        self.process.stdin.flush()
+
+    def start(self):
+        # 여기에서 프로세스를 시작하고 초기화 합니다.
+        self.process = subprocess.Popen(
+            [self.path, "bob.c"], 
+            stdin=subprocess.PIPE, 
+            stdout=subprocess.PIPE, 
+            #stderr=subprocess.PIPE,  # 컴파일 과정에서의 출력 무시
+            text=True
+        )
+        success = self.fork_handshake()
+        if not success:
+            print("Handshake failed!")
+            exit()
+
+    def compile(self, source_code):
+        self.sync_write_data(source_code)
+        result_json_str = self.process.stdout.readline()
+        return json.loads(result_json_str)
+    
+    def fork_handshake(self) -> bool:
+        ret = self.process.stdout.readline()
+        if ret not in "fork client hello\n" : 
+            print("fork client hello failed")
+            return False
+        self.sync_write_data("fork server hello\n")
+        ret = self.process.stdout.readline()
+        if ret not in "done\n" :
+            print("fork server hello failed\n")
+            return False
+        # set timeout
+        self.sync_write_data(self.compile_time_out)
+        ret = self.process.stdout.readline()
+        if "time_out_set" not in ret:
+            print("failed to set time out\n")
+            return False 
+        #print("success set time out!")
+        return True
+
+
+
+analyze_queue = queue.Queue()
+
+# 종료 신호를 처리하기 위한 이벤트 객체 생성
+shutdown_event = threading.Event()
+
+def signal_handler(signum, frame):
+    print("Signal received, shutting down")
+    shutdown_event.set()
+
+# 시그널 핸들러 설정
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+
+def analyze_results_thread():
+    while not shutdown_event.is_set():
+        if not analyze_queue.empty():
+            data = analyze_queue.get()
+            analyze_results(*data)
+        else:
+            time.sleep(5)
+
+def fuzzer_init():
+    gcc_compiler = Compiler(name="gcc", path="./gcc-trunk")
+    clang_compiler = Compiler(name="clang", path="./clang-18")
+    compilers = [gcc_compiler, clang_compiler]
+
+    # 컴파일러를 시작
+    for compiler in compilers:
+        compiler.start() 
+
+    setup_output_dirs(generators)
+    machine_info = get_machine_info()
+    analysis_thread = threading.Thread(target=analyze_results_thread, daemon=True)
+    analysis_thread.start()
+
+    return (compilers, machine_info, analysis_thread)#, generate_thread=None)
+
+
+if __name__ == "__main__":
+
+    compilers, machine_info, analysis_thread =  fuzzer_init()
+
+    while True:
+        input_src = ""
+        generator_name = None
+
+        results = compile_and_run(compilers, id, generator_name, input_src)
+        result_data = (generator_name, id, results, machine_info)
+        analyze_queue.put(result_data)
+    
+    #analysis_thread.join()
+        
+
+
+
+
+
+# run_thread -> code generator
+# run_thread -> analyzer
+# anlyzer qeue.get() <- 여기서 스핀락인지? 아니면 성능에 영향이 없는지 확인 필ㄹ요할듯
+# get() -> here: jmp here
+# compiler start / gcc, clang(process형태로 연결)
+# compliers = [gcc_compiler, clang_compiler]
+# source_code = code_queue.get()
+# 병렬로
+# results['gcc'] = gcc_compiler.compile(source_code)
+# results['c;ang'] = clang_compiler.compile(source_code)
+# results 받기
+# analyzer.put(result)
+# anaylzer queue로 결과 전송
