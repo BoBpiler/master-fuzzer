d30f76aa69d51693b5649e66343334ab07473161
Gyumin Baek <guminb@ajou.ac.kr>
2023-10-15 16:24:47 +0900

    modified

--- inst_deps.bat
+++ inst_deps.bat
@@ -1,6 +1,6 @@
 @echo off
-copy src\csmith\m4.exe c:\windows\system32
-copy src\csmith\regex2.dll c:\windows\system32
+copy csmith\m4.exe c:\windows\system32
+copy csmith\regex2.dll c:\windows\system32
 
 pip install progressbar
 pip install requests
