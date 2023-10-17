308964a0b67834025bb9a8879c166d8205300daa
Gyumin Baek <guminb@ajou.ac.kr>
2023-10-17 11:00:15 +0900

    wasm_install.sh script completed

--- src/wasm_install.sh
+++ src/wasm_install.sh
@@ -2,20 +2,30 @@
 
 set -e
 
+# 경고 메시지: sudo 사용
+echo "NOTE: This script might require your password to use 'sudo' for certain installations."
+
+# Wasmer 설치
 echo "Installing Wasmer..."
 curl https://get.wasmer.io -sSfL | sh
 
 echo "Copying Wasmer binary to /usr/bin..."
-sudo cp /$HOME/.wasmer/bin/wasmer /usr/bin/wasmer
+sudo cp "$HOME/.wasmer/bin/wasmer" /usr/bin/wasmer
 
+# Wasmtime 설치
 echo "Installing Wasmtime..."
 curl https://wasmtime.dev/install.sh -sSf | bash
 
 echo "Copying Wasmtime binary to /usr/bin..."
-sudo cp /$HOME/.wasmtime/bin/wasmtime /usr/bin/wasmtime
+sudo cp "$HOME/.wasmtime/bin/wasmtime" /usr/bin/wasmtime
 
+# Node 설치
 echo "Installing node..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+if [ ! -d "$HOME/.nvm" ]; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+else
+    echo "nvm already installed!"
+fi
 
 # nvm 환경 설정 불러오기
 export NVM_DIR="$HOME/.nvm"
@@ -25,7 +35,9 @@ nvm install node  # 최신 버전 설치
 nvm use node  # 최신 버전 사용
 
 # emsdk 설치
-git clone https://github.com/emscripten-core/emsdk.git
+if [ ! -d "emsdk" ]; then
+    git clone https://github.com/emscripten-core/emsdk.git
+fi
 cd emsdk
 
 # 최신 버전의 Emscripten 툴체인 설치 및 활성화
@@ -46,9 +58,9 @@ add_to_file_if_not_present() {
 }
 
 # .bashrc와 .zshrc에 내용 추가
-add_to_file_if_not_present ~/.bashrc "source $EMSDK_PATH"
-add_to_file_if_not_present ~/.zshrc "source $EMSDK_PATH"
+add_to_file_if_not_present ~/.bashrc ". $EMSDK_PATH"
+add_to_file_if_not_present ~/.zshrc ". $EMSDK_PATH"
 
-source $EMSDK_PATH
+. $EMSDK_PATH
 echo "Configuration updated!"
 cd ../
