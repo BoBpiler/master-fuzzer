6dc939c638c7acebaf6cfcdb540c24b56c7baaf4
Gyumin Baek <guminb@ajou.ac.kr>
2023-10-20 01:30:55 +0900

    fuzzer display for windows

--- inst_deps.bat
+++ inst_deps.bat
@@ -5,5 +5,6 @@ copy csmith\regex2.dll c:\windows\system32
 pip install progressbar
 pip install requests
 pip install psutil
+pip install windows-curses
 
 ssh-keygen -t rsa -b 4096 -f "%~dp0BoBpiler" -N "BoBpiler BoBpiler"
